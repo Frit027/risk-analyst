468ea3a13f5c2ee70a64cf534e2f301b83ac7684
Frit027 <Frit_027@mail.ru>
2021-10-10 21:48:31 +0300

    added real statistics

---
+++ static/js/statistics_script.js
@@ -0,0 +1,37 @@
+let statusCanvas = document.getElementById("statusChart");
+let pieChart = new Chart(statusCanvas, {
+    type: 'pie',
+    data: {},
+    options: { responsive: false }
+});
+
+$( "#my_button" ).click(function() {
+    let sol_id = parseInt($('#select-solution').val())
+    let cat_id = parseInt($('#select-category').val())
+    let req_id = parseInt($('#select-req').val())
+    let cond_id = parseInt($('#select-cond').val())
+
+    $.post( "/data", { sol_id: sol_id, cat_id: cat_id, req_id: req_id, cond_id:cond_id }, function(data) {
+        pieChart.data = {
+            labels: [
+                "Удовлетворено",
+                "Удовлетворено частично",
+                "Отказано"
+            ],
+            datasets: [
+                {
+                    data: [
+                        data.satisfied_percent,
+                        data.partially_satisfied_percent,
+                        data.denied_percent
+                    ],
+                    backgroundColor: [
+                        "#32CD32",
+                        "#FFA420",
+                        "#F80000"
+                    ]
+                }]
+        };
+        pieChart.update();
+    }, "json");
+});
