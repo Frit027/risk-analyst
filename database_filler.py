37e709414013f3ad8e929242f2dcc9d018422ada
Frit027 <Frit_027@mail.ru>
2021-10-10 21:41:42 +0300

    added filling of the database

--- database_filler.py
+++ database_filler.py
@@ -1,6 +1,8 @@
-from app import Solution, Category, Condition, Requirement
+from app import Solution, Category, Condition, Requirement, Deal, Document
 from constants import SolutionType
 import csv
+import re
+import sys
 
 
 class DatabaseFiller:
@@ -15,18 +17,74 @@ class DatabaseFiller:
 
     @staticmethod
     def fill_category(db):
-        with open('static/categories.csv', encoding='utf-8') as f:
+        with open('static/csv/categories.csv', encoding='utf-8') as f:
             for name in f:
                 db.session.add(Category(name=name.rstrip()))
 
     @staticmethod
     def fill_requirements(db):
-        with open('static/requirements.csv', encoding='utf-8') as f:
+        with open('static/csv/requirements.csv', encoding='utf-8') as f:
             for data in csv.reader(f):
                 db.session.add(Requirement(name=data[0]))
 
     @staticmethod
     def fill_conditions(db):
-        with open('static/conditions.csv', encoding='utf-8') as f:
+        with open('static/csv/conditions.csv', encoding='utf-8') as f:
             for data in csv.reader(f):
                 db.session.add(Condition(name=data[0]))
+
+    @staticmethod
+    def fill(db):
+        pattern = re.compile(r'.[\d-]+/')
+
+        with open('static/csv/data.csv', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            previous_deal_id = ''
+
+            for i, row in enumerate(reader):
+                doc_id = row['FullPath'][19:]
+                deal_id = pattern.search(doc_id).group(0)[:-1]
+                doc_solution_name = row['LOC_SOL']
+                deal_solution_name = row['SOL_GLOB']
+                inst = int(row['Instance'])
+                cond_str = row['COND_1']
+                req_str = row['REQ_1']
+                cat_lst = []
+                if row['CAT_1']:
+                    cat_lst.append(row['CAT_1'])
+                if row['CAT_2']:
+                    cat_lst.append(row['CAT_2'])
+                if row['CAT_3']:
+                    cat_lst.append(row['CAT_3'])
+
+                req = Requirement.query.filter_by(name=req_str).first()
+                cond = Condition.query.filter_by(name=cond_str).first()
+
+                doc_solution = Solution.query.filter_by(name=doc_solution_name).first()
+                deal_solution = Solution.query.filter_by(name=deal_solution_name).first()
+
+                doc = Document(id=doc_id, req_detail=req_str, cond_detail=cond_str, instance=inst,
+                               requirements=[req], conditions=[cond])
+                doc_solution.documents.append(doc)
+                req.documents.append(doc)
+                cond.documents.append(doc)
+
+                categories = [Category.query.filter_by(name=name).first() for name in cat_lst]
+
+                if deal_id != previous_deal_id:
+                    deal = Deal(id=deal_id, categories=categories, documents=[doc])
+                    deal_solution.deals.append(deal)
+                    [category.deals.append(deal) for category in categories]
+
+                    db.session.add(deal)
+                else:
+                    deal = Deal.query.get(deal_id)
+                    deal.documents.append(doc)
+                    db.session.add(deal)
+
+                previous_deal_id = deal_id
+
+                db.session.add(doc)
+
+                # print(f"{i}:{'': <2}{deal_id: <18}{doc_id: <33}{doc_status: <20}{deal_status: <24}{cond: <24}{req: <24}",
+                #       file=sys.stdout)
