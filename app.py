0804ed923e8a64ca8e2bd6ed491ba8b59d5e0e44
Frit027 <Frit_027@mail.ru>
2021-10-10 21:40:45 +0300

    added statistics calculation

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-import random
+from constants import SolutionType
 import sys
 
 
@@ -25,7 +25,7 @@ class Category(db.Model):
 
     deals = db.relationship('Deal',
                             secondary=category_deal,
-                            back_populates='categories') # backref=db.backref('categories_deals', lazy='dynamic'))
+                            back_populates='categories')
 
 
 class Deal(db.Model):
@@ -34,7 +34,7 @@ class Deal(db.Model):
 
     categories = db.relationship(Category,
                                  secondary=category_deal,
-                                 back_populates='deals') # backref=db.backref('deals_categories', lazy='dynamic'))
+                                 back_populates='deals')
     documents = db.relationship('Document', backref='deal_documents', lazy='dynamic')
 
 
@@ -63,7 +63,7 @@ document_condition = db.Table(
 class Document(db.Model):
     id = db.Column(db.String(255), primary_key=True)
     req_detail = db.Column(db.Text, nullable=False)
-    con_detail = db.Column(db.Text, nullable=False)
+    cond_detail = db.Column(db.Text, nullable=False)
     instance = db.Column(db.Integer, nullable=False)
     deal_id = db.Column(db.String(255), db.ForeignKey('deal.id'))
     solution_id = db.Column(db.Integer, db.ForeignKey('solution.id'))
@@ -71,11 +71,9 @@ class Document(db.Model):
     requirements = db.relationship('Requirement',
                                    secondary=document_requirement,
                                    back_populates='documents')
-                                   # backref=db.backref('documents_requirements', lazy='dynamic'))
     conditions = db.relationship('Condition',
                                  secondary=document_condition,
                                  back_populates='documents')
-                                 # backref=db.backref('documents_conditions', lazy='dynamic'))
 
 
 class Requirement(db.Model):
@@ -85,7 +83,6 @@ class Requirement(db.Model):
     documents = db.relationship(Document,
                                 secondary=document_requirement,
                                 back_populates='requirements')
-                                # backref=db.backref('requirements_documents', lazy='dynamic'))
 
 
 class Condition(db.Model):
@@ -95,36 +92,83 @@ class Condition(db.Model):
     documents = db.relationship(Document,
                                 secondary=document_condition,
                                 back_populates='conditions')
-                                # backref=db.backref('conditions_documents', lazy='dynamic'))
 
 
-db.create_all()
+# db.create_all()
 
 
 @app.route('/')
 def index():
-    from database_filler import DatabaseFiller
+    # from database_filler import DatabaseFiller
     # DatabaseFiller.fill_solution(db)
     # DatabaseFiller.fill_category(db)
     # DatabaseFiller.fill_requirements(db)
     # DatabaseFiller.fill_conditions(db)
-
+    # DatabaseFiller.fill(db)
 
     db.session.commit()
 
-    return render_template('index.html')
+    return render_template('index.html',
+                           solutions=Solution.query.order_by(Solution.name).all(),
+                           categories=Category.query.order_by(Category.name).all(),
+                           requirements=Requirement.query.order_by(Requirement.name).all(),
+                           conditions=Condition.query.order_by(Condition.name).all())
 
 
 @app.route('/data', methods=['GET', 'POST'])
-def send_data():
+def send_statistics():
     if request.method == 'POST':
-        id = request.form['category']
+        solution_id = request.form['sol_id']
+        category_id = request.form['cat_id']
+        requirement_id = request.form['req_id']
+        condition_id = request.form['cond_id']
 
-        num1, num2 = [random.randint(1, 49) for _ in range(2)]
+        print(f'{solution_id} {category_id} {requirement_id} {condition_id}', file=sys.stdout)
 
-        return {'satisfied_percent': num1,
-                'partially_satisfied_percent': num2,
-                'denied_percent': 100 - (num1 + num2)}
+        deals = get_deals(solution_id, category_id, requirement_id, condition_id)
+        if not deals:
+            return {'response': None}
+
+        return {'satisfied_percent': get_percent(deals, SolutionType.SATISFIED.value),
+                'partially_satisfied_percent': get_percent(deals, SolutionType.PARTIALLY_SATISFIED.value),
+                'denied_percent': get_percent(deals, SolutionType.DENIED.value)}
+
+
+def get_deals(solution_id, category_id, requirement_id, condition_id):
+    lst = []
+    if solution_id != 'NaN':
+        s = set(deal.id for deal in Solution.query.get(solution_id).deals)
+        print('ID дел данного решения:', s, file=sys.stdout)
+        lst.append(s)
+    if category_id != 'NaN':
+        s = set(deal.id for deal in Category.query.get(category_id).deals)
+        print('ID дел данной категории:', s, file=sys.stdout)
+        lst.append(s)
+    if requirement_id != 'NaN':
+        s = set(doc.deal_id for doc in Requirement.query.get(requirement_id).documents)
+        print('ID дел данного требования:', s, file=sys.stdout)
+        lst.append(s)
+    if condition_id != 'NaN':
+        s = set(doc.deal_id for doc in Condition.query.get(condition_id).documents)
+        print('ID дел данного обстоятельства:', s, file=sys.stdout)
+        lst.append(s)
+
+    if lst:
+        res = lst[0]
+        for ids in lst:
+            res.intersection_update(ids)
+        print('Пересечение:', res, file=sys.stdout)
+        return [Deal.query.get(id) for id in res]
+    return []
+
+
+def get_percent(deals, type):
+    k = 0
+    for deal in deals:
+        solution = Solution.query.get(deal.solution_id).name
+        if solution == type:
+            k += 1
+    return k / len(deals) * 100
 
 
 if __name__ == '__main__':
