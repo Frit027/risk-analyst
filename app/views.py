3abfd2087a196d7bbd225a8fda61a65a641d685e
Frit027 <Frit_027@mail.ru>
2021-10-18 10:15:51 +0300

    added new post views

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from app.utils.txt_parser import TxtParser
 from app.constants import SolutionType
 from app import app, db
 from flask import render_template, request
-import csv
+import jsonpickle
 import sys
 
 
@@ -24,21 +24,71 @@ def index():
 
     return render_template('index.html',
                            solutions=Solution.query.order_by(Solution.name).all(),
-                           categories=Category.query.order_by(Category.name).all(),
+                           categories=get_categories(), # Category.query.order_by(Category.name).all(),
                            requirements=Requirement.query.order_by(Requirement.name).all(),
                            conditions=Condition.query.order_by(Condition.name).all()
                            )
 
 
-def get_new_conds():
-    with open('app/static/csv/bankruptcyCondListItog.csv', encoding='utf-8') as f:
-        return [Condition.query.filter_by(name=row[0]).first() for row in csv.reader(f)]
+@app.route('/documents', methods=['GET', 'POST'])
+def documents_page():
+    if request.method == 'POST':
+        solution_id = request.form.get('sol_id')
+        category_ids = tuple(map(int, request.form.getlist('cat_ids')))
+        requirement_id = request.form.get('req_id')
+        condition_id = request.form.get('cond_id')
+
+        print(f'solution_id = {solution_id}', file=sys.stdout)
+        print(f'category_ids = {category_ids}', file=sys.stdout)
+        print(f'requirement_id = {requirement_id}', file=sys.stdout)
+        print(f'condition_id = {condition_id}', file=sys.stdout)
+        res = EntitiesIntersection.get_docs(solution_id, category_ids, requirement_id, condition_id)
+
+        return render_template('documents_list.html',
+                               solutions=Solution.query.order_by(Solution.name).all(),
+                               categories=get_categories(), # Category.query.order_by(Category.name).all(),
+                               requirements=Requirement.query.order_by(Requirement.name).all(),
+                               conditions=Condition.query.order_by(Condition.name).all(),
+                               docs=res
+                               )
+    return render_template('documents_list.html',
+                           solutions=Solution.query.order_by(Solution.name).all(),
+                           categories=get_categories(), # Category.query.order_by(Category.name).all(),
+                           requirements=Requirement.query.order_by(Requirement.name).all(),
+                           conditions=Condition.query.order_by(Condition.name).all()
+                           )
+
+
+@app.route('/document/<string:doc_id>', methods=['GET'])
+def document_page(doc_id):
+    deal_id = doc_id.split('_')[0]
+    doc_id = doc_id.replace('_', '/')
+
+    text_doc = TxtParser.f(doc_id)
+
+    deal = Deal.query.get(deal_id)
+    doc = Document.query.get(doc_id)
+    categories = deal.categories
+    deal_solution = Solution.query.get(deal.solution_id).name
+    doc_solution = Solution.query.get(doc.solution_id).name
+
+    return render_template('detail_document.html',
+                           documents=deal.documents,
+                           cur_doc=doc,
+                           category=categories[0],
+                           deal_solution=deal_solution,
+                           doc_solution=doc_solution,
+                           text=text_doc
+                           )
 
 
 @app.route('/data', methods=['POST'])
 def send_statistics():
     if request.method == 'POST':
-        deals = EntitiesIntersection.get_deals(request.form['cat_id'], request.form['req_id'], request.form['cond_id'])
+        deals = EntitiesIntersection.get_deals(request.form.getlist('cat_ids[]'),
+                                               request.form['req_id'],
+                                               request.form['cond_id']
+                                               )
 
         if not deals:
             return {'response': None}
@@ -49,58 +99,19 @@ def send_statistics():
                 }
 
 
-@app.route('/documents', methods=['GET', 'POST'])
-def documents_page():
-    if request.method == 'POST':
-        print('here', file=sys.stdout)
-        solution_id = request.form.get('sol_id')
-        category_id = request.form.get('cat_id')
-        requirement_id = request.form.get('req_id')
-        condition_id = request.form.get('cond_id')
-        print(category_id, requirement_id, condition_id, file=sys.stdout)
-
-        ids1 = set(doc for deal in Category.query.get(category_id).deals for doc in deal.documents)
-        ids2 = set(doc for doc in Requirement.query.get(requirement_id).documents)
-        ids3 = set(doc for doc in Condition.query.get(condition_id).documents)
-        res = list(ids1 & ids2 & ids3)
-        print(res, file=sys.stdout)
-
-        return render_template('documents_list.html',
-                               solutions=Solution.query.order_by(Solution.name).all(),
-                               categories=Category.query.order_by(Category.name).all(),
-                               requirements=Requirement.query.order_by(Requirement.name).all(),
-                               conditions=Condition.query.order_by(Condition.name).all(),
-                               docs=res
-                               )
-    return render_template('documents_list.html',
-                           solutions=Solution.query.order_by(Solution.name).all(),
-                           categories=Category.query.order_by(Category.name).all(),
-                           requirements=Requirement.query.order_by(Requirement.name).all(),
-                           conditions=Condition.query.order_by(Condition.name).all()
-                           )
+@app.route('/req_cond', methods=['POST'])
+def send_req_cond():
+    return jsonpickle.encode(EntitiesIntersection.get_req_cond_by_categories(request.form.getlist('cat_ids[]')))
 
 
-@app.route('/document/<string:doc_id>', methods=['GET'])
-def document_page(doc_id):
-    deal_id = doc_id.split('_')[0]
-    doc_id = doc_id.replace('_', '/')
-    print(f'Deal: {deal_id}, doc: {doc_id}', file=sys.stdout)
+@app.route('/all_req_cond', methods=['POST'])
+def send_all_req_cond():
+    return jsonpickle.encode({'requirements': Requirement.query.order_by(Requirement.name).all(),
+                              'conditions': Condition.query.order_by(Condition.name).all()}
+                             )
 
-    text_doc = TxtParser.f(doc_id)
 
-    deal = Deal.query.get(deal_id)
-    doc = Document.query.get(doc_id)
-    categories = deal.categories
-    deal_solution = Solution.query.get(deal.solution_id).name
-    doc_solution = Solution.query.get(doc.solution_id).name
-
-    print(f'Deal1: {deal.id}, doc1: {doc.id}, {doc.req_detail}', file=sys.stdout)
-
-    return render_template('detail_document.html',
-                           documents=deal.documents,
-                           cur_doc=doc,
-                           category=categories[0],
-                           deal_solution=deal_solution,
-                           doc_solution=doc_solution,
-                           text=text_doc
-                           )
+def get_categories():
+    return [Category.query.filter_by(name='Банкротство гражданина').first(),
+            Category.query.filter_by(name='Аренда земли').first(),
+            Category.query.filter_by(name='Поставка электроэнергии').first()]
