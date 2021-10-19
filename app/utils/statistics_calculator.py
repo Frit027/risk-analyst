c2ff409bb6423983c74d6e9fa6344b9b345d9aeb
Frit027 <Frit_027@mail.ru>
2021-10-20 01:47:59 +0300

    moved function

--- app/utils/statistics_calculator.py
+++ app/utils/statistics_calculator.py
@@ -1,12 +1,24 @@
 from app.models import Solution
+from app.utils.entities_intersection import EntitiesIntersection
+from app.constants import SolutionType
 
 
 class StatisticsCalculator:
     @staticmethod
-    def get_percent(deals, type):
+    def __get_percent(deals, type):
         k = 0
         for deal in deals:
             solution = Solution.query.get(deal.solution_id).name
             if solution == type:
                 k += 1
         return k / len(deals) * 100
+
+    @staticmethod
+    def get_percents(cat_ids, req_ids, cond_ids):
+        deals = EntitiesIntersection.get_deals(cat_ids, req_ids, cond_ids)
+        if not deals:
+            return {'response': None}
+        return {'satisfied_percent': StatisticsCalculator.__get_percent(deals, SolutionType.SATISFIED.value),
+                'partially_satisfied_percent': StatisticsCalculator.__get_percent(deals, SolutionType.PARTIALLY_SATISFIED.value),
+                'denied_percent': StatisticsCalculator.__get_percent(deals, SolutionType.DENIED.value)
+                }
