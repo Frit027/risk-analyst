3a5e70edbf2c64af2befd2a845d87c8644d4c67b
Frit027 <Frit_027@mail.ru>
2021-10-20 01:48:12 +0300

    moved function

--- app/static/js/statistics_script.js
+++ app/static/js/statistics_script.js
@@ -3,32 +3,43 @@ let pieChart = new Chart($("#statusChart"), {
     options: { responsive: false }
 });
 
+if (typeof data !== 'undefined') {
+    show_diagram(data)
+    console.log(data);
+} else {
+    console.log(123);
+}
+
 $( "#show_statistics_button" ).click(function() {
     let cat_id = $('#select-category').val().map(Number)
-    let req_id = parseInt($('#select-req').val())
-    let cond_id = parseInt($('#select-cond').val())
+    let req_id = $('#select-req').val().map(Number)
+    let cond_id = $('#select-cond').val().map(Number)
 
-    $.post( "/data", { cat_ids: cat_id, req_id: req_id, cond_id:cond_id }, function(data) {
-        pieChart.data = {
-            labels: [
-                "Удовлетворено",
-                "Удовлетворено частично",
-                "Отказано"
-            ],
-            datasets: [
-                {
-                    data: [
-                        data.satisfied_percent,
-                        data.partially_satisfied_percent,
-                        data.denied_percent
-                    ],
-                    backgroundColor: [
-                        "#32CD32",
-                        "#FFA420",
-                        "#F80000"
-                    ]
-                }]
-        };
-        pieChart.update();
+    $.post( "/data", { cat_ids: cat_id, req_ids: req_id, cond_ids:cond_id }, function(data) {
+        show_diagram(data)
     }, "json");
 });
+
+function show_diagram(data) {
+    pieChart.data = {
+        labels: [
+            "Удовлетворено",
+            "Удовлетворено частично",
+            "Отказано"
+        ],
+        datasets: [
+            {
+                data: [
+                    data.satisfied_percent,
+                    data.partially_satisfied_percent,
+                    data.denied_percent
+                ],
+                backgroundColor: [
+                    "#32CD32",
+                    "#FFA420",
+                    "#F80000"
+                ]
+            }]
+    };
+    pieChart.update();
+}
