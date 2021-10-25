8b1b93e6b1615162d1a940364aaef39298761dc2
Frit027 <Frit_027@mail.ru>
2021-10-25 17:40:18 +0300

    added deals sorting

--- app/utils/entities_intersection.py
+++ app/utils/entities_intersection.py
@@ -1,5 +1,4 @@
 from app.models import Category, Condition, Requirement, Deal, Solution
-import sys
 
 
 class EntitiesIntersection:
@@ -8,22 +7,18 @@ class EntitiesIntersection:
         lst = []
         if len(category_ids):
             s = set(deal.id for id in category_ids for deal in Category.query.get(id).deals)
-            # print('IDs дел данных категорий:', s, file=sys.stdout)
             lst.append(s)
         if len(requirement_ids):
             s = set(doc.deal_id for id in requirement_ids for doc in Requirement.query.get(id).documents)
-            # print('IDs дел данных требований:', s, file=sys.stdout)
             lst.append(s)
         if len(condition_ids):
             s = set(doc.deal_id for id in condition_ids for doc in Condition.query.get(id).documents)
-            # print('IDs дел данных обстоятельств:', s, file=sys.stdout)
             lst.append(s)
 
         if lst:
             res = lst[0]
             for ids in lst:
                 res.intersection_update(ids)
-            # print('Пересечение дел:', res, file=sys.stdout)
             return [Deal.query.get(id) for id in res]
         return []
 
@@ -32,31 +27,28 @@ class EntitiesIntersection:
         lst = []
         if len(category_ids):
             s = set(doc for id in category_ids for deal in Category.query.get(id).deals for doc in deal.documents)
-            # print(f'category_ids = {category_ids}', file=sys.stdout)
-            print('Доки данной категории:', s, file=sys.stdout)
             lst.append(s)
         if len(requirement_ids):
             s = set(doc for id in requirement_ids for doc in Requirement.query.get(id).documents)
-            # print(f'requirement_ids = {requirement_ids}', file=sys.stdout)
-            print('Доки данного требования:', s, file=sys.stdout)
             lst.append(s)
         if len(condition_ids):
             s = set(doc for id in condition_ids for doc in Condition.query.get(id).documents)
-            # print(f'condition_ids = {condition_ids}', file=sys.stdout)
-            print('Доки данного обстоятельства:', s, file=sys.stdout)
             lst.append(s)
         if len(solution_ids):
             docs = set(doc for id in solution_ids for doc in Solution.query.get(id).documents)
-            print('Доки данного решения:', docs, file=sys.stdout)
-            # print(f'solution_ids = {solution_ids}', file=sys.stdout)
             lst.append(docs)
 
         if lst:
             res = lst[0]
-            for docs in lst:
-                res.intersection_update(docs)
-            print('Пересечение доков:', res, file=sys.stdout)
-            return res
+            [res.intersection_update(docs) for docs in lst]
+            sort_docs = sorted(res, key=lambda x: x.id)
+
+            r = []
+            if len(sort_docs):
+                [r.append(sort_docs[i]) for i in range(0, len(sort_docs) - 1)
+                 if sort_docs[i].deal_id != sort_docs[i + 1].deal_id]
+                r.append(sort_docs[-1])
+            return r
         return []
 
     @staticmethod
